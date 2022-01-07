ca367e5ccfd7e8d37666e5705791730977060880
pp-byte <81913245+pp-byte@users.noreply.github.com>
2022-01-07 11:25:40 +0530

    Update settings.py

--- table_form/settings.py
+++ table_form/settings.py
@@ -209,11 +209,4 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'prachipareek862@gmail.com'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'Pr@chi1234'
-EMAIL_USE_SSL= False
 
